cc259c5ef58e7d3f1e7a9b3ffa05758b5ea8d1e8
dzatorski <damian2222221@gmail.com>
2022-08-07 11:38:34 +0200

    Whole console game logic done

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 const game = () => {
+  let playerScore = 0;
+  let cpuScore = 0;
   for (let i = 0; i < 5; i++) {
     const gameArray = [`paper`, `scissors`, `rock`];
     let cpuChoice = gameArray[Math.floor(Math.random() * gameArray.length)];
     let playerChoice = String(prompt(`Write rock, paper or scissors`));
     let player = String(playerChoice).toLowerCase();
     if (cpuChoice === `rock` && player === `rock`) {
-      console.log(`It is a draw`);
     } else if (cpuChoice === `rock` && player === `paper`) {
-      console.log(`You won`);
+      playerScore++;
     } else if (cpuChoice === `rock` && player === `scissors`) {
-      console.log(`You lost`);
+      cpuScore++;
     } else if (cpuChoice === `paper` && player === `paper`) {
-      console.log(`Draw`);
     } else if (cpuChoice === `paper` && player === `scissors`) {
-      console.log(`You won`);
+      playerScore++;
     } else if (cpuChoice === `paper` && player === `rock`) {
-      console.log(`You lost`);
+      cpuScore++;
     } else if (cpuChoice === `scissors` && player === `scissors`) {
-      console.log(`Draw`);
     } else if (cpuChoice === `scissors` && player === `paper`) {
-      console.log(`You lost`);
+      cpuScore++;
     } else if (cpuChoice === `scissors` && player === `rock`) {
-      console.log(`You won`);
+      playerScore++;
     }
   }
+  if (playerScore > cpuScore) {
+    console.log(`You won ${playerScore} to ${cpuScore}`);
+  } else if (playerScore === cpuScore) {
+    console.log(`You drew ${playerScore} to ${cpuScore}`);
+  } else if (playerScore < cpuScore) {
+    console.log(`You lost ${playerScore} to ${cpuScore}`);
+  }
 };
-
 game();
