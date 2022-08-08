49561f61dfc56a123cb3caf7e6d21ff02fc7ebc6
dzatorski <damian2222221@gmail.com>
2022-08-08 14:26:21 +0200

    Game logic done, changed design to be symmetrical

--- script.js
+++ script.js
@@ -35,32 +35,75 @@ const game = () => {
 game();
 */
 // Playing a game
-
 const result = document.querySelector(`.won-lose`);
+const pointsCounter = document.querySelector(`.results`);
+const gameArray = [`paper`, `scissors`, `rock`];
+let points = [0, 0];
+pointsCounter.textContent = `${points[0]} : ${points[1]}`;
+const showPoints = () => {
+  pointsCounter.textContent = `${points[0]} : ${points[1]}`;
+  if (points[0] > 4) {
+    pointsCounter.textContent = `You won ${points[0]} : ${points[1]}`;
+  } else if (points[1] > 4) {
+    pointsCounter.textContent = `You lost ${points[0]} : ${points[1]}`;
+  }
+};
 
+const showCpuChoice = () => {
+  if (cpuChoice === `rock`) {
+    document.getElementById(`cpu`).src = "./images/fist.png";
+  } else if (cpuChoice === `paper`) {
+    document.getElementById(`cpu`).src = "./images/palm-of-hand.png";
+  } else if (cpuChoice === `scissors`) {
+    document.getElementById(`cpu`).src = "./images/scissor.png";
+  }
+};
 const gamePlay = () => {
-  const gameArray = [`paper`, `scissors`, `rock`];
   let cpuChoice = gameArray[Math.floor(Math.random() * gameArray.length)];
   console.log(cpuChoice);
-  if (playerChoice === `rock` && cpuChoice === `rock`) {
-    result.textContent = `It's a draw`;
-  } else if (playerChoice === `rock` && cpuChoice === `paper`) {
-    result.textContent = `You lost this round`;
-  } else if (playerChoice === `rock` && cpuChoice === `scissors`) {
-    result.textContent = `You won this round`;
-  } else if (playerChoice === `paper` && cpuChoice === `scissors`) {
-    result.textContent = `You lost this round`;
-  } else if (playerChoice === `paper` && cpuChoice === `rock`) {
-    result.textContent = `You won this round`;
-  } else if (playerChoice === `paper` && cpuChoice === `paper`) {
-    result.textContent = `It's a draw`;
-  } else if (playerChoice === `scissors` && cpuChoice === `scissors`) {
-    result.textContent = `It's a draw`;
-  } else if (playerChoice === `scissors` && cpuChoice === `paper`) {
-    result.textContent = `You won this round`;
-  } else if (playerChoice === `scissors` && cpuChoice === `rock`) {
-    result.textContent = `You lost this round`;
-  }
+  if (points[0] < 5 && points[1] < 5) {
+    if (playerChoice === `rock` && cpuChoice === `rock`) {
+      result.textContent = `It's a draw`;
+      showPoints();
+      showCpuChoice();
+    } else if (playerChoice === `rock` && cpuChoice === `paper`) {
+      result.textContent = `You lost this round`;
+      points[1] += 1;
+      showPoints();
+      showCpuChoice();
+    } else if (playerChoice === `rock` && cpuChoice === `scissors`) {
+      result.textContent = `You won this round`;
+      points[0] += 1;
+      showPoints();
+      showCpuChoice();
+    } else if (playerChoice === `paper` && cpuChoice === `scissors`) {
+      result.textContent = `You lost this round`;
+      points[1] += 1;
+      showPoints();
+      showCpuChoice();
+    } else if (playerChoice === `paper` && cpuChoice === `rock`) {
+      result.textContent = `You won this round`;
+      points[0] += 1;
+      showPoints();
+      showCpuChoice();
+    } else if (playerChoice === `paper` && cpuChoice === `paper`) {
+      result.textContent = `It's a draw`;
+      showCpuChoice();
+    } else if (playerChoice === `scissors` && cpuChoice === `scissors`) {
+      result.textContent = `It's a draw`;
+      showCpuChoice();
+    } else if (playerChoice === `scissors` && cpuChoice === `paper`) {
+      result.textContent = `You won this round`;
+      points[0] += 1;
+      showPoints();
+      showCpuChoice();
+    } else if (playerChoice === `scissors` && cpuChoice === `rock`) {
+      result.textContent = `You lost this round`;
+      points[1] += 1;
+      showPoints();
+      showCpuChoice();
+    }
+  } else result.textContent = `Game ended`;
 };
 
 let playerChoice = ``;
