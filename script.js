bd26f1ea955194d954ab2251a67c82aeae2a8527
dzatorski <damian2222221@gmail.com>
2022-08-08 13:04:12 +0200

    Basic dom done

--- script.js
+++ script.js
@@ -34,3 +34,50 @@ const game = () => {
 };
 game();
 */
+// Playing a game
+
+const result = document.querySelector(`.won-lose`);
+
+const gamePlay = () => {
+  const gameArray = [`paper`, `scissors`, `rock`];
+  let cpuChoice = gameArray[Math.floor(Math.random() * gameArray.length)];
+  console.log(cpuChoice);
+  if (playerChoice === `rock` && cpuChoice === `rock`) {
+    result.textContent = `It's a draw`;
+  } else if (playerChoice === `rock` && cpuChoice === `paper`) {
+    result.textContent = `You lost this round`;
+  } else if (playerChoice === `rock` && cpuChoice === `scissors`) {
+    result.textContent = `You won this round`;
+  } else if (playerChoice === `paper` && cpuChoice === `scissors`) {
+    result.textContent = `You lost this round`;
+  } else if (playerChoice === `paper` && cpuChoice === `rock`) {
+    result.textContent = `You won this round`;
+  } else if (playerChoice === `paper` && cpuChoice === `paper`) {
+    result.textContent = `It's a draw`;
+  } else if (playerChoice === `scissors` && cpuChoice === `scissors`) {
+    result.textContent = `It's a draw`;
+  } else if (playerChoice === `scissors` && cpuChoice === `paper`) {
+    result.textContent = `You won this round`;
+  } else if (playerChoice === `scissors` && cpuChoice === `rock`) {
+    result.textContent = `You lost this round`;
+  }
+};
+
+let playerChoice = ``;
+const playerChoiceRock = document.querySelector(`.rock`);
+playerChoiceRock.addEventListener(`click`, () => {
+  playerChoice = `rock`;
+  gamePlay();
+});
+
+const playerChoicePaper = document.querySelector(`.paper`);
+playerChoicePaper.addEventListener(`click`, () => {
+  playerChoice = `paper`;
+  gamePlay();
+});
+
+const playerChoiceScissors = document.querySelector(`.scissors`);
+playerChoiceScissors.addEventListener(`click`, () => {
+  playerChoice = `scissors`;
+  gamePlay();
+});
